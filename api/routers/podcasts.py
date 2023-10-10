b4d10728196fcb5ad0084bbff3c22b8e901a9a2b
I T <ivo.a.v.tavares@gmail.com>
2023-10-10 10:51:03 +0100

    added post endpoint to add podcasts to postgres db.

--- api/routers/podcasts.py
+++ api/routers/podcasts.py
@@ -4,7 +4,10 @@ from typing import Optional
 from pathlib import Path
 from api.utilities.constants import podcasts_dict
 from fastapi import APIRouter, Depends,HTTPException, status
-from pydantic_models.podcasts import Podcast as Podcast_Pydantic
+from pydantic_models.podcasts import (
+    Podcast as Podcast_Pydantic,
+    PodcastOut as Podcast_Pydantic_Out
+)
 from database.connecting_orm import database_gen
 from sqlalchemy.orm import Session
 from sqlalchemy import select
@@ -12,7 +15,7 @@ from database.schemas import Podcasts as Podcasts_SQL
 
 
 router = APIRouter(prefix="/podcasts", tags=["Podcasts"])
-@router.get('/list', 
+@router.get('/', 
             status_code=status.HTTP_200_OK, 
             response_model=list[Podcast_Pydantic]
 )
@@ -42,3 +45,41 @@ async def list_podcasts(
         # }
     return podcasts_list
 
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=Podcast_Pydantic_Out)
+def add_podcast(
+    payload: Podcast_Pydantic,
+    db_session: Session = Depends(database_gen)
+):
+    """
+    Adding a podcast to podcast table in postgres db.
+    """
+    # creating a data according to schema
+    new_podcast = Podcasts_SQL(**(payload.dict()))
+    # checking for already existing podcast
+    stmt =(
+        select(Podcasts_SQL)
+        .where(
+            Podcasts_SQL.title==new_podcast.title,
+            Podcasts_SQL.url==new_podcast.url
+        )
+    )
+    podcasts_list = [
+        # row.t is a tuple whose 1st element is a schema instance
+        Podcast_Pydantic.from_schema_to_pydantic(row.t[0])
+        for row in db_session.execute(stmt)
+    ]
+    if podcasts_list == []:
+        # adding data to session, moving it to pending state.
+        db_session.add(new_podcast)
+        # moving all data in pending state, in this session, to persistant state.
+        db_session.commit()
+        # update new_post with data returned from db_session
+        db_session.refresh(new_podcast)
+        return new_podcast
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=
+            f"A podcast with title = {new_podcast.title}, "
+            f"or url = {new_podcast.url}, already exists!",
+        )
--- pydantic_models/podcasts.py
+++ pydantic_models/podcasts.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from pathlib import Path
-from database.schemas import Podcasts 
+from database.schemas import Podcasts
+import datetime
 
 class Podcast(BaseModel):
     """
@@ -17,3 +18,23 @@ class Podcast(BaseModel):
             title=podcast.title, url=podcast.url, folder=podcast.folder,
             language=podcast.language
         )
+class PodcastOut(Podcast):
+    """
+    Pydantic class that represents a Podcast
+    """
+    id: int
+    created_at: datetime.datetime
+    
+    class Config:
+        """
+        Behaviour of pydantic can be controlled via the Config class on a model
+        or a pydantic dataclass.
+        Pydantic's `orm_mode` will tell the Pydantic model to read the data
+        even if it is not a dict, but an ORM model (or any other arbitrary 
+        object with attributes). With this, the Pydantic model is compatible with ORMs, 
+        and you can just declare it in the response_model argument in your path operations.
+        You will be able to return a database model and it will read the data from it.
+        https://fastapi.tiangolo.com/tutorial/response-model/?h=
+        """
+
+        orm_mode = True
