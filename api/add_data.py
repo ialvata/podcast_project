79e756b9885b6389abb0e9a4e46055383907d8dd
I T <ivo.a.v.tavares@gmail.com>
2023-10-29 08:25:21 +0000

    added update to episodes table to add_data.py

--- api/add_data.py
+++ api/add_data.py
@@ -7,20 +7,24 @@ from database.connecting_orm import database_gen, engine
 from database.utilities import init_db
 from scrappers.france_culture import scrape_france_culture
 from database.mongodb import MongoDB
+from database.from_mongodb_to_postgres import transfer_france_culture_from_mongodb_to_postgres
 
 
 #####################    Creating some initial data in Postgres db    #######################
 # Base.metadate.create_all should be always in the main.py, from where we run the app.
 schemas.Base.metadata.create_all(bind=engine, checkfirst=True)
-get_initial_db = next(database_gen())
-if get_initial_db.query(schemas.Podcasts).all() == []:
+postgres_db_session = next(database_gen())
+if postgres_db_session.query(schemas.Podcasts).all() == []:
     print(f"Creating Podcasts table in {schemas.Base}")
-    init_db(get_initial_db)
+    init_db(postgres_db_session)
 # scrapping data
 print("Scrapping data")
-documents = scrape_france_culture(get_initial_db)
+documents = scrape_france_culture(postgres_db_session)
 # sending episode data to MongoDB
-db = MongoDB(filename="./database/database.ini", section="mongodb")
-db.connect()
+mongo_db = MongoDB(filename="./database/database.ini", section="mongodb")
+mongo_db.connect()
 print("Sending data to MongoDB")
-db.send_files(database="podcast_project", collection="episodes",list_files= documents)
+mongo_db.send_files(database="podcast_project", collection="episodes",list_files= documents)
+print("Updating Episodes table in Postgres")
+transfer_france_culture_from_mongodb_to_postgres(sql_db_session = postgres_db_session,
+                                                 mongo = mongo_db)
