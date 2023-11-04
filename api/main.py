0a2f2db0e5c8ba8a3fea367453e4480d54f2402c
I T <ivo.a.v.tavares@gmail.com>
2023-11-04 16:58:58 +0000

    serving index.html from FastAPI

--- api/main.py
+++ api/main.py
@@ -6,13 +6,17 @@ import uvicorn
 from fastapi import FastAPI
 from api.routers import podcasts, episodes
 from fastapi.middleware.cors import CORSMiddleware
-
-
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from pathlib import Path
 ##############################    Creatng FastAPI API   ##########################
 api = FastAPI()
 origins = [
     "*",
 ]
+##########                Setting CORS     #####################
 api.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -20,12 +24,19 @@ api.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@api.get("/")
-async def main():
-    return {"message": "Hello World"}
+################                   Adding Backend Endpoints                ####################
 api.include_router(podcasts.router)
 api.include_router(episodes.router)
 
+################                   Serving Webpage                ####################
+static_path = Path("./frontend/static").absolute()
+templates = Jinja2Templates(directory="./frontend")
+api.mount(str(static_path), StaticFiles(directory=str(static_path)), name="static")
+
+
+@api.get("/", response_class=HTMLResponse)
+async def render_webpage(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run("main:api", host="localhost", port=8001, reload=True)
